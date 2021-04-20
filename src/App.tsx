f72794a43f8ca72e86a8042219e46909024c5213
MichaelRyanGoad <michael.ryan.goad@gmail.com>
2021-04-20 17:43:29 -0400

    Added cube + gh-page code

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ThreeExample from "./Three/threeExample";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Experience the Cube</p>
+        <ThreeExample />
       </header>
     </div>
   );
